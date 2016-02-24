0b713b33733cd29b74569f23cdbe48f5ec84edf8
luish000 <luish000.lh@gmail.com>
2016-02-24 11:21:43 -0430

    added exceptions handler

--- quarks/index.js
+++ quarks/index.js
@@ -1,6 +1,7 @@
 module.exports = [
   require('proton-quark-configurations'),
   require('proton-quark-controllers'),
+  require('proton-quark-exceptions'),
   require('proton-quark-policies'),
   require('proton-quark-services'),
   require('proton-quark-models'),
